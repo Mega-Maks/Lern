ed66cf22ca31d88ee0a18e251905a9016e634ae3
Максим Рыжков <ryzkov0@yandex.ru>
2021-01-23 15:41:14 +0500

    var chenges

--- Учёба_2/Распространённые_форматы_текстовых_файлов-CSV,JSON.py
+++ Учёба_2/Распространённые_форматы_текстовых_файлов-CSV,JSON.py
@@ -1,20 +1,25 @@
 import csv
-Dict = {}
-List = []
+
+Dict = dict()
+List = list
 Max = 0
+
 def Dictwriter(key):
     if key in Dict:
         Dict[key] += 1
     else:
         Dict[key] = 1
-with open('Crimes.csv') as g:
-    reader = csv.reader(g)
+
+with open('Crimes.csv') as file:
+    reader = csv.reader(file)
     for row in reader:
         if row[2][8:10] == "15":
             Dictwriter(row[5])
+
 for i in Dict:
     print(i, Dict[i])
     if Dict[i] > Max:
         Max = Dict[i]
         key = i
+
 print(key, Max)
