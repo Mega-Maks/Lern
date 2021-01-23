d9e513fbba4185903bb12b1f5623b10278580d7f
Максим Рыжков <ryzkov0@yandex.ru>
2021-01-23 15:37:23 +0500

    Выделение пробелами

--- Учёба_2/Распространённые_форматы_текстовых_файлов-CSV,JSON_2.py
+++ Учёба_2/Распространённые_форматы_текстовых_файлов-CSV,JSON_2.py
@@ -2,30 +2,38 @@ import json
 Dict = {}
 flip_dict = {}
 Set = set()
+
 def Dictwriter(key, value, Dict):
     if key in Dict:
         Dict[key] += [value]
     else:
         Dict[key] = [value]
+
 def Counter(Name):
     if Name in flip_dict:
         for parent in flip_dict[Name]:
             Set.add(parent)
             Counter(parent)
     return len(Set)
+
 def Flip_dict():
     for elem in List:
         if elem['parents'] != []:
             for i in elem['parents']:
                 Dictwriter(i, elem['name'], flip_dict)
+
 with open('file.json', 'w') as g:
     g.write(input())
+
 with open('file.json') as g:
     List = json.load(g)
+
 Flip_dict()
+
 for i in List:
     if i["name"] not in flip_dict:
         flip_dict[i["name"]] = []
+
 for i in sorted(flip_dict):
     print(i, ':', Counter(i) + 1)
     Set.clear()
